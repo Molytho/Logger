bc6be02b27ef87ff584595033daf629ff0e93641
Robin Ebert <ebert-robin@web.de>
2020-01-29 08:03:54 +0100

    Added threadsynchronization to LogfileHandler

--- Logger/LogfileHandler.cs
+++ Logger/LogfileHandler.cs
@@ -20,16 +20,24 @@ namespace Molytho.Logger
                 throw new ArgumentException("One log way needs to be chosen");
         }
 
+        #region File
         private FileInfo File { get; }
         private FileStream FileStream { get; }
         private StreamWriter StreamWriter { get; }
-
+        #endregion
+        private readonly object syncRoot = new object();
         private readonly bool bToSTDOUT;
 
         public void WriteLogMessage<T>(LogMessage<T> message) where T : System.Enum
         {
             string printMessage = message.ToString();
-            StreamWriter?.WriteLine(printMessage);
+            if(StreamWriter != null)
+            {
+                lock(syncRoot)
+                {
+                    StreamWriter.WriteLine(printMessage);
+                }
+            }
             if(bToSTDOUT)
                 Console.Out.WriteLine(printMessage);
         }
