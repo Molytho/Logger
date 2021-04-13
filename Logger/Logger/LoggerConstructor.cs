443cde6f2f4f0576379bf7e64729100fdf3052ce
Robin Ebert <ebertrobin2002@gmail.com>
2021-04-13 14:49:20 +0200

    Cleanup Logger constructors

--- Logger/Logger/LoggerConstructor.cs
+++ Logger/Logger/LoggerConstructor.cs
@@ -8,34 +8,33 @@ namespace Molytho.Logger
     public partial class Logger<T>
         where T : Enum
     {
+
         [DebuggerHidden]
         public Logger(bool toSTDOUT = true, ILogMessageFormater<T> messageFormater = null, params string[] logFilePath)
-            : this()
         {
             ILogMessageFormater<T> formater = messageFormater ?? DefaultLogMessageFormater<T>.Instance;
             TextWriter[] logFiles = new TextWriter[logFilePath.Length + (toSTDOUT ? 1 : 0)];
-            for(int i = 0; i < logFilePath.Length; i++)
-            {
-                logFiles[i] = new StreamWriter(logFilePath[i], true);
-            }
 
+            for(int i = 0; i < logFilePath.Length; i++)
+                logFiles[i] = new StreamWriter(logFilePath[i], true);
             if(toSTDOUT)
                 logFiles[logFilePath.Length] = Console.Out;
+
             fileHandler = new LogfileHandler<T>(formater, logFiles);
+            startTime = Now;
+            T[] enumTypes = (T[])Enum.GetValues(typeof(T));
+            logEvents = new Dictionary<T, Action<LogMessage<T>>>(enumTypes.Length);
+            foreach(var item in enumTypes)
+                logEvents.Add(item, default);
         }
+
         [DebuggerHidden]
         public Logger(ILogMessageFormater<T> messageFormater = null, params TextWriter[] pWriterStreams)
-            : this()
         {
             ILogMessageFormater<T> formater = messageFormater ?? DefaultLogMessageFormater<T>.Instance;
+
             fileHandler = new LogfileHandler<T>(formater, pWriterStreams);
-        }
-
-        [DebuggerHidden]
-        private Logger()
-        {
             startTime = Now;
-
             T[] enumTypes = (T[])Enum.GetValues(typeof(T));
             logEvents = new Dictionary<T, Action<LogMessage<T>>>(enumTypes.Length);
             foreach(var item in enumTypes)
