42304a242c7a282ac70641337cdf9963d3358bd7
Robin Ebert <ebertrobin2002@gmail.com>
2021-04-13 10:49:22 +0200

    Use record for LogMessage

--- Logger/LogMessage.cs
+++ Logger/LogMessage.cs
@@ -4,23 +4,12 @@ using System.Text;
 
 namespace Molytho.Logger
 {
-    public readonly struct LogMessage<T>
+    public record LogMessage<T>(T Type, string Message, TimeSpan Time)
         where T : Enum
     {
-        public LogMessage(T type, string message, TimeSpan time)
-        {
-            Type = type;
-            Message = message;
-            Time = time;
-        }
-
-        public T Type { get; }
-        public string Message { get; }
-        public TimeSpan Time { get; }
-
-        public readonly override string ToString()
+        public override string ToString()
             => ToString(DefaultLogMessageFormater<T>.Instance, null);
-        public readonly string ToString(ILogMessageFormater<T> formater, object formatProviderData)
+        public string ToString(ILogMessageFormater<T> formater, object formatProviderData)
             => formater.FormateLogMessage(this, formatProviderData);
     }
 }
